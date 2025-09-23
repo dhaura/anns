82320875cdb7cbbaf0ecb4bcfc776de3e0fb9476
dhaura <dhaurapathirana@gmail.com>
2025-09-22 21:50:02 -0700

    Fix executable path in sbatch script.

--- hnsw/sparse_pyramid/scripts/full.sh
+++ hnsw/sparse_pyramid/scripts/full.sh
@@ -9,4 +9,4 @@
 
 export OMP_NUM_THREADS=__CPUS_PER_TASK__
 
-srun -n __TASKS_PER_NODE__ --cpu-bind=cores ./build/sparse_pyramid $SCRATCH/datasets/SpKNN/grassRMA/base_full.csr __SAMPLE_SIZE__ 12 __K__ 16 200 $SCRATCH/datasets/SpKNN/grassRMA/queries.dev.csr $SCRATCH/datasets/SpKNN/grassRMA/base_full.dev.gt
+srun -n __TASKS_PER_NODE__ --cpu-bind=cores $SCRATCH/benchmarks/SpKNN/anns/hnsw/sparse_pyramid/build/sparse_pyramid $SCRATCH/datasets/SpKNN/grassRMA/base_full.csr __SAMPLE_SIZE__ 12 __K__ 16 200 $SCRATCH/datasets/SpKNN/grassRMA/queries.dev.csr $SCRATCH/datasets/SpKNN/grassRMA/base_full.dev.gt
